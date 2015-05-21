28a0e71e192b2c491d89432f3440485913ee9ee5
Jmorros <morros_2468@hotmail.com>
2015-05-21 23:24:51 +0200

    Practica 4.2

---
+++ Anaologic_4.2/Anaologic_4.2.ino
@@ -0,0 +1,48 @@
+/*************************************************************************  
+**                                                                      **
+**                            Examen Arduino                            **                                               
+**                                                                      **
+*************************************************************************/
+
+//Primer: Valor del potenciometre
+//Segon: Encendre tots els LEDs (8)
+//Tercer: Els LEDs han de fer blink, amb funció del A0
+//Quart: Cotxe fantastic, (········) de tres amb tres
+
+
+//*******  Includes  *****************************************************
+
+
+//*******  Variables  ****************************************************
+int Spk= 3;
+int POT1= A0;
+int POT2= A1;
+int ValPot1;
+int ValPot2;
+
+//*******  Setup  ********************************************************
+void setup(){  
+   Serial.begin (9600);
+   pinMode (Spk, OUTPUT);
+   pinMode (POT1, INPUT); 
+   pinMode (POT1, INPUT); 
+
+}
+
+
+//*******  Loop  *********************************************************
+void loop(){
+  
+  ValPot1= analogRead (POT1);
+  ValPot1= map (ValPot1, 0, 1023, 100, 3000);
+  tone (Spk, 10);
+  delay(ValPot1);
+  
+  ValPot2= analogRead (POT2);
+  ValPot2= map (ValPot2, 0, 1023, 100, 3000);
+  noTone (Spk);
+  delay(ValPot2);
+
+  
+}
+
